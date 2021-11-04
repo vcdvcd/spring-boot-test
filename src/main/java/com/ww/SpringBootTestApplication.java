6c12ee3488fd881f277086df0679d5736a2fcf07
ww <vcdbrovcd@ww.com>
2021-11-04 18:39:40 +0800

    second commit

--- src/main/java/com/ww/SpringBootTestApplication.java
+++ src/main/java/com/ww/SpringBootTestApplication.java
@@ -2,12 +2,13 @@ package com.ww;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBootTestApplication.class, args);
     }
 
 }
